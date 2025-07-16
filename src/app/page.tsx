29ea8dc1b93db7b26004ee8391c5470cad76fb8a
Tsvetanmarkov <tsvetan.markov.a70@learn.telerikacademy.com>
2025-07-16 16:45:20 +0300

    switch places user icon and theme selector

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,14 @@ export default function Home() {
   return (
     <div className="mt-10 ml-10">
       <div className="flex flex-row items-center gap-5 justify-end px-5 py-5">
+        <ModeToggle></ModeToggle>
+
         <SignedOut>
           <SignInButton></SignInButton>
         </SignedOut>
         <SignedIn>
           <UserButton></UserButton>
         </SignedIn>
-        <ModeToggle></ModeToggle>
       </div>
     </div>
   );
