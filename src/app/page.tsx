22309485d5ce2bf15c60ab4dfa7bb46dce922dd9
Tsvetanmarkov <tsvetan.markov.a70@learn.telerikacademy.com>
2025-07-16 16:44:28 +0300

    add theme provider light dark and system

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
+import { ModeToggle } from "@/components/ModeToggle";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="mt-10 ml-10">
-      <SignedOut>
-        <SignInButton></SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton></UserButton>
-      </SignedIn>
+      <div className="flex flex-row items-center gap-5 justify-end px-5 py-5">
+        <SignedOut>
+          <SignInButton></SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton></UserButton>
+        </SignedIn>
+        <ModeToggle></ModeToggle>
+      </div>
     </div>
   );
 }
